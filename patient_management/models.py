baa31bf5cdce3823e54fcd39296e72bdbf0a9b12
enoskyeza <enoskyeza@gmail.com>
2022-06-24 06:46:54 +0300

    implemented all requested changes to model.py

--- patient_management/models.py
+++ patient_management/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 from core.models import BaseModel
+# from django_countries.fields import CountryField
 
 # Create your models here.
 class Patient(BaseModel):
-    surname = models.CharField(max_length=15)
-    given_name = models.CharField(max_length=15)
+    surname = models.CharField(max_length=50)
+    given_name = models.CharField(max_length=50)
     age = models.PositiveIntegerField()
-    gender = models.CharField(max_length=2)
-    Nationality = models.CharField(max_length=10)
-    patient_id = models.CharField(max_length=15)
-    address =models.CharField(max_length=15)
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female')
+    ]
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    nationality = models.CharField(max_length=50, null=True)
+    identification = models.CharField(max_length=50, blank=True)
+    address =models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return '{} {}'.format(self.surname, self.given_name)
-        
+
 
