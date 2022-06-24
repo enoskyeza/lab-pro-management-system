9162cd58653915572117945f1ade178014c7d61d
enoskyeza <enoskyeza@gmail.com>
2022-06-24 08:34:15 +0300

    modified the templates and urls to match the views

--- patient_management/urls.py
+++ patient_management/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('list', views.PatientListView.as_view(), name='patient-list'),
-    # path('<int:pk>/', views.PatientDetailView.as_view(), name='patient-details'),
-    path('new/', views.create_patient, name='create-patient'),
-    path('update/', views.update_patient, name='update-patient'),
+    path('<int:pk>/', views.PatientDetailView.as_view(), name='patient-details'),
+    path('new/', views.PatientCreateView.as_view(), name='create-patient'),
+    path('update/<int:pk>/', views.PatientUpdateView.as_view(), name='update-patient'),
 ]
