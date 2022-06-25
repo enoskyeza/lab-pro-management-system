0771a04ded9eba51ddc44c24167704196e7e92da
enoskyeza <enoskyeza@gmail.com>
2022-06-25 12:15:35 +0300

    added lab management app to settings and urls

--- lap_pro/urls.py
+++ lap_pro/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('patient/', include('patient_management.urls')),
+    path('test/', include('lab_management.urls')),
     path('admin/', admin.site.urls),
 ]
