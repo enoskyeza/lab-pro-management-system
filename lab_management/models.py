a8b561f260c2e2c2893b33070842f3d679cf10e0
enoskyeza <enoskyeza@gmail.com>
2022-06-25 12:23:41 +0300

    created the test model for lab management app

---
+++ lab_management/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from core.models import BaseModel
+
+# Create your models here.
+class Test(BaseModel):
+    name = models.CharField(max_length=50)
+    price = models.CharField(max_length=50)
+    duration = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '{} {}'.format(self.name)
+
