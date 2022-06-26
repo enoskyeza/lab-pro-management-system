6129101245ab890f69ce2a99b95bad8c1fe9a80b
enoskyeza <enoskyeza@gmail.com>
2022-06-26 11:57:38 +0300

    added requests and sample, model forms

--- lab_management/forms.py
+++ lab_management/forms.py
@@ -1,8 +1,17 @@
 from django.forms import ModelForm 
-from lab_management.models import Test
+from lab_management.models import Test, TestRequest, Sample
 
 class TestForm(ModelForm):
     class Meta:
         model = Test
         exclude = ('created_at', 'deleted_at', 'is_deleted','updated_at')
-        
+
+class SampleForm(ModelForm):
+    class Meta:
+        model = Sample
+        exclude = ('created_at', 'deleted_at', 'is_deleted','updated_at')
+
+class TestRequestForm(ModelForm):
+    class Meta:
+        model = TestRequest
+        exclude = ('created_at', 'deleted_at', 'is_deleted','updated_at')
