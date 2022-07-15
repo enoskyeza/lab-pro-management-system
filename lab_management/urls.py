798b890f69fb178001363d4fb79bad2e1c0d7a23
enoskyeza <enoskyeza@gmail.com>
2022-07-16 00:44:40 +0300

    pass req-id to add it automaticaly to new sample

--- lab_management/urls.py
+++ lab_management/urls.py
@@ -18,6 +18,6 @@ urlpatterns = [
     path('requests/<int:pk>/', TestRequestDetailView.as_view(), name='request-details'),
     path('requests/', TestRequestListView.as_view(), name='requests-list'),
     path('requestupdate/<int:pk>', TestRequestUpdateView.as_view(), name='update-request'),
-    path('sample/', SampleCreateView.as_view(), name='create-sample'),
+    path('sample/<int:pk>', SampleCreateView.as_view(), name='create-sample'),
 ]
 
